cb927b59b0a1c4226e9109fc8159fe25b5267b62
tanilserbes <serbestanil@gmail.com>
2020-04-08 00:56:03 +0100

    ex num9 done

--- directory.rb
+++ directory.rb
@@ -17,8 +17,13 @@ def input_students
     input = gets.chomp #EX 7
     if input == "K" #EX 7
       students << {name: name, cohort: :november, hobbie: hobbie } #added hobbies ex.5 #EX 7
-      puts "Now we have #{students.count} students"
+      if students.count > 1 #EX NUM 9
+       puts "Now we have #{students.count} students"
+      else
+        puts "Now we have #{students.count} student"
+      end
     end #EX 7
+
     #get another name from the user
     puts "Enter the student name"
     name = gets.chomp
@@ -31,12 +36,12 @@ puts "The students of Villains Academy"
 puts "-------------"
 end
 def print(names)
-  names.each_with_index do |student, number| #number before the name of each student
+  #names.each_with_index do |student, number| #number before the name of each student
     #puts "#{number+1}. #{student[:name].center(10)} (#{student[:cohort]} cohort)" #exercises 0, 1 # center() method try  ex. 6
     #puts student[:name] if student[:name][0].upcase == "A" #print the student whose name begins with "a" letter, ex. 2
     #puts student[:name] if student[:name].gsub(/\s+/, "").length < 12 #  print the students whose name is shorter than 12 characters. ex 3
-    puts "#{number+1}. #{student[:name].center(10)} #{student[:cohort]} cohort #{student[:hobbie]} "
-  end
+    #puts "#{number+1}. #{student[:name].center(10)} #{student[:cohort]} cohort #{student[:hobbie]} " # EX 7
+  #end
 
   #=begin # ex.4 prints all students using while
   #loop_number = 0
@@ -45,10 +50,13 @@ def print(names)
   #  loop_number += 1
   #end
   #=end
-
-
-
-
+number =0
+  names.map do |student|
+    if student[:hobbie ] == 'basket'
+      puts "#{number+1}. #{student[:name].center(10)} (#{student[:cohort]} cohort)" #ex num8 instead of cohort, ordered in  hobbies
+      number += 1
+    end
+   end
 end
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
