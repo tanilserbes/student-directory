5a7eeec3aecedfdc7b38fc58c0367328462ce0c0
tanilserbes <serbestanil@gmail.com>
2020-04-08 12:26:41 +0100

    created save file

--- typos.rb
+++ typos.rb
@@ -10,6 +10,7 @@ def print_menu
   # 1. print the menu and ask the user what to do
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -27,6 +28,8 @@ def process(selection)
       @students = input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -66,5 +69,17 @@ end
 
 def print_footer
   puts "Overall, we have #{@students.count} great students"
+end
+
+def save_students
+  #open the file for writing
+  file = File.open("students.csv", "w")
+  #iterate over the array of save_students
+  @students.each do |student|
+    student_data = [student[:name],student[:cohort]]
+    cvs_line = student_data.join(",")
+    file.puts cvs_line
+  end
+  file.close
 end
 interactive_menu
